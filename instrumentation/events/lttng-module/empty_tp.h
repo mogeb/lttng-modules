0ed2263a4cb3316197c50515de4975ed5bf8c8d7
Mohamad Gebai <mohamad.gebai@gmail.com>
2016-02-16 12:56:36 -0500

    Added more sizes of empty tracepoint
    Signed-off-by: Mohamad Gebai <mohamad.gebai@gmail.com>

--- instrumentation/events/lttng-module/empty_tp.h
+++ instrumentation/events/lttng-module/empty_tp.h
@@ -7,12 +7,34 @@
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM empty_tp
 
-LTTNG_TRACEPOINT_EVENT(empty_ioctl_1b,
+LTTNG_TRACEPOINT_EVENT(empty_ioctl_4b,
 			TP_PROTO(int payload),
-	    TP_ARGS(payload),
+		TP_ARGS(payload),
 
 	TP_FIELDS(
 		ctf_integer(int, payload, payload)
+	)
+)
+
+LTTNG_TRACEPOINT_EVENT(empty_ioctl_8b,
+			TP_PROTO(int p1, int p2),
+		TP_ARGS(p1, p2),
+
+	TP_FIELDS(
+		ctf_integer(int, p1, p1)
+		ctf_integer(int, p2, p2)
+	)
+)
+
+LTTNG_TRACEPOINT_EVENT(empty_ioctl_16b,
+			TP_PROTO(int p1, int p2, int p3, int p4),
+		TP_ARGS(p1, p2, p3, p4),
+
+	TP_FIELDS(
+		ctf_integer(int, p1, p1)
+		ctf_integer(int, p2, p2)
+		ctf_integer(int, p3, p3)
+		ctf_integer(int, p4, p4)
 	)
 )
 
